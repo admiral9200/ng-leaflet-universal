a9fbd24429875a31d7d2a4bbe531a888f3b0e237
Nicholas Wong <lion9200.world@gmail.com>
2021-08-30 16:17:54 -0400

    [ADD] index in module

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { NgLeafletUniversalModule } from './src/lib/ng-leaflet-universal.module';
+export { MapComponent } from './src/lib/map/map.component';
+export { MapService } from './src/lib/services/map.service';
+export { BridgeService } from './src/lib/services/bridge.service';
