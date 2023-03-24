d58dd3121adf86499ecbd340f07faa49733cfd84
Nicholas Wong <lion9200.world@gmail.com>
2023-03-24 14:07:52 -0400

    refactor: improve library even more - used more functional approach - removed unnecessary elements - utilize lib for box calculations

--- projects/ng-leaflet-universal/src/lib/map.component.ts
+++ projects/ng-leaflet-universal/src/lib/map.component.ts
@@ -1,11 +1,12 @@
-import { AfterViewInit, Input, Output, EventEmitter } from '@angular/core';
 import { map, tileLayer, divIcon, Marker as LeafletMarker } from 'leaflet';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { OnChanges, SimpleChanges } from '@angular/core';
+import { FeatureGroup } from 'leaflet';
 import type { Map } from 'leaflet';
 
-import { calculateCenterBoundingBox, calculateZoom } from './utils';
-import { distanceBetweenCoordinates, generate } from './utils';
-import { Marker, MarkerCard } from './models';
+import { generate, getCardHtml } from './utils';
+import { Marker } from './models';
 
 @Component({
   selector: 'ng-leaflet-universal',
@@ -19,17 +20,21 @@ import { Marker, MarkerCard } from './models';
   styleUrls: ['./map.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MapComponent implements AfterViewInit {
+export class MapComponent implements AfterViewInit, OnChanges {
   @Output() mapEvent = new EventEmitter<Marker>();
-  @Input() markers: Array<Marker>;
+  @Input() markers: Array<Marker> | null;
 
   id = generate('div');
-  maxDistance = 0;
-
   map: Map;
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.markers && this.map) {
+      this.updateMarkers(this.markers);
+    }
+  }
+
   ngAfterViewInit() {
-    this.map = map(this.id).setView([0, 0], 1);
+    this.map = map(this.id).setView([18.4, -66.9], 4);
 
     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data © OpenStreetMap contributors',
@@ -38,102 +43,60 @@ export class MapComponent implements AfterViewInit {
     this.updateMarkers(this.markers);
   }
 
-  updateMarkers(markers: Array<Marker>) {
+  updateMarkers(markers: Array<Marker> | null) {
     if (!markers) return;
 
-    const { center, minimum, maximum } = calculateCenterBoundingBox(markers);
-
-    markers.forEach((item) => this.addMarker(item));
-
-    this.maxDistance = distanceBetweenCoordinates(minimum, maximum);
-    this.map.setView(center, calculateZoom(this.maxDistance));
-  }
-
-  displayCard(data: Marker, selected: LeafletMarker) {
-    if (!data?.card) return;
-
-    const html = this.getCardHtml(data.card);
-    const popup = selected.getPopup();
-
-    if (popup) popup?.openPopup();
-    else {
-      selected
-        .bindPopup(html, {
-          autoClose: false,
-          maxWidth: 200,
-        })
-        .openPopup();
-    }
-  }
-
-  addMarker(marker: Marker) {
-    const leafletMarker = new LeafletMarker({
-      lat: marker.location.latitude,
-      lng: marker.location.longitude,
+    const leafletMarkers = markers.map(createLeafletMarker).map((marker, i) => {
+      return marker.on('click', () => this.mapEvent.emit(markers[i]));
     });
 
-    const icon = divIcon({
+    const group = new FeatureGroup(leafletMarkers);
+
+    group.addTo(this.map);
+
+    this.map.fitBounds(group.getBounds());
+  }
+}
+
+function createLeafletMarker(marker: Marker) {
+  const leafletMarker = new LeafletMarker({
+    lat: marker.location.latitude,
+    lng: marker.location.longitude,
+  });
+
+  leafletMarker.setIcon(
+    divIcon({
       html:
         marker.html ||
         `
-        <div class="item-marker">
-            <div class="icon-image" style="background-image: url('${marker.icon}')">
-            </div>
-        </div>`,
+      <div class="item-marker">
+          <div class="icon-image" style="background-image: url('${marker.icon}')">
+          </div>
+      </div>`,
       className: 'map-marker-icon',
       iconSize: [26, 30],
       iconAnchor: [13, 30],
-    });
+    })
+  );
 
-    leafletMarker.setIcon(icon);
+  leafletMarker.on('click', function ({ target, latlng }) {
+    if (marker.cardActivated && marker?.card) {
+      const html = getCardHtml(marker.card);
+      const popup = target.getPopup();
 
-    leafletMarker.addTo(this.map).on('click', () => {
-      this.mapEvent.emit(marker);
-
-      if (marker.cardActivated) {
-        const { latitude, longitude } = marker.location;
-
-        this.displayCard(marker, leafletMarker);
-
-        this.map.setView([latitude, longitude], 15);
+      if (popup) popup?.openPopup();
+      else {
+        target
+          .bindPopup(html, {
+            autoClose: false,
+            maxWidth: 200,
+          })
+          .openPopup();
       }
-    });
-  }
+    }
 
-  getCardHtml(card: MarkerCard): string {
-    if (card?.customHtml) return card.customHtml;
+    target._map.setView(latlng, 15);
+  });
 
-    return /* html */ `
-      <div class="map-card">
-        <div class="map-card-body">
-          <div class="top-card">
-            <img src="${card?.image?.url}" alt="${card?.title?.text}" />
-            <div class="content">
-              <h2 class="map-card-title">${card?.title?.text}</h2>
-              <h4 class="map-card-subtitle">${card?.subtitle?.text}</h4>
-              <h5 class="map-address">${card?.address?.text}</h5>
-            </div>
-          </div>
-          <div class="map-card-content">
-            ${card?.content?.text}
-          </div>
-          <div class="cta-wrapper">
-            ${card?.callToActions
-              ?.map(
-                (cta) => `
-                <a href="${cta.link}"
-                  target="_blank"
-                  style="
-                  background-color: ${cta.backgroundColor || '#007FFF'};
-                  color: ${cta.textColor || 'white'};"
-                  class="map-card-cta">
-                    ${cta.icon ? `<i class="${cta.icon}"></i>` : ''}
-                ${cta.text}</a>
-                `
-              )
-              .reduce((a, b) => a + b)}
-            </div>
-        </div>
-      </div>`;
-  }
+  return leafletMarker;
 }
